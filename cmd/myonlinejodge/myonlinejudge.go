4c339a17c37c17fa6e0a6f24871a4523ad0734c4
DanielHenry <daniel.henry.39@gmail.com>
2019-04-05 17:09:59 +0700

    Add Hello World in myonlinejudge.go

--- cmd/myonlinejodge/myonlinejudge.go
+++ cmd/myonlinejodge/myonlinejudge.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := routes.setup();
-	// Listen and Server in 0.0.0.0:8090
-	r.Run(":8090");
+	fmt.Printf("Hello World!\n")
 }
