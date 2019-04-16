f775e4076ba3f41fddc72f25aae98c763c9be5aa
DanielHenry <daniel.henry.39@gmail.com>
2019-04-16 15:17:13 +0700

    Test main.go

--- cmd/myonlinejodge/main.go
+++ cmd/myonlinejodge/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	//"fmt"
+	//"net/http"
 	"github.com/gin-gonic/gin"
+	"github.com/DanielHenry/my-online-judge/internal/pkg/controller/questionscontroller"
+	"github.com/DanielHenry/my-online-judge/internal/pkg/controller/userscontroller"
 )
 
 func main() {
-	fmt.Printf("Hello World!\n")
+	router := gin.Default()
+
+	v1 := router.Group("/api/v1/users")
+	{
+		v1.POST("/", userscontroller.create)
+		v1.GET("/", userscontroller.index)
+		v1.GET("/:id", userscontroller.show)
+		v1.PUT("/:id", userscontroller.update)
+		v1.DELETE("/:id", userscontroller.destroy)
+	}
+	router.Run("127.0.0.1:8090")
 }
